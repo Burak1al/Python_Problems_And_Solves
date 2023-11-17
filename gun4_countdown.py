3671efc93a03510fcd55dcd98a84cf8e24a5a5cd
Burak Albay <148315804+Burak1al@users.noreply.github.com>
2023-11-17 14:32:49 +0300

    Add files via upload

---
+++ gun4_countdown.py
@@ -0,0 +1,4 @@
+number = int(input('num-> '))
+while number >= 0:
+    print(number)
+    number -= 1
---
+++ gun4_hackerrank.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    n = int(input())
+    if n in range(1, 21):
+        for i in range(0, n):
+            print(i**2)
